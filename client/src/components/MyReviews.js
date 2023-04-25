128697e9629b84fe274d0448c329bdb34775fb6d
brandongiordano <bgiordano9917@aol.com>
2023-04-24 20:57:11 -0400

    added responsive reviews/list feature to profile

---
+++ client/src/components/MyReviews.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+const WatchList = () => {
+    return (
+    <container fluid>
+      <Row xs={2} md={5} className="g-4">
+        {Array.from({ length: 7 }).map((_, idx) => (
+          <Col style={{justifyContent: 'center', display: 'flex'}}>
+            <Card style={{ width: '12rem', height: '18rem', }}>
+              <Card.Img variant="top" src="https://via.placeholder.com/150" />
+              <Card.ImgOverlay>
+              <Card.Body>
+                <Card.Title>Card title</Card.Title>
+              </Card.Body>
+              </Card.ImgOverlay>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </container>
+    );
+  }
+
+export default WatchList;
