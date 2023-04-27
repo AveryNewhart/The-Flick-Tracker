b3c80a72ab8e34aae624f735a0ce6f7f31f44835
brandongiordano <bgiordano9917@aol.com>
2023-04-27 18:51:30 -0400

    new feed features

--- client/src/components/Dashboard/DashFeed.js
+++ client/src/components/Dashboard/DashFeed.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
@@ -7,7 +8,19 @@ const DashFeed = () => {
 
     return (
         <div>
-            <h1>Dash Feed Test</h1>
+            <Container />
+             <Row>
+                <Col>
+                    {/* <img className='movie__poster' src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} 
+                    style={{
+                    width: "35%",
+                    float: "left"
+                    }}/> */}
+                    <img src='https://via.placeholder.com/150'/>
+              </Col>
+                <Col xs={8}style={{backgroundColor: 'blue'}}></Col>
+             </Row>
+            <Container />
         </div>
     )
 }
