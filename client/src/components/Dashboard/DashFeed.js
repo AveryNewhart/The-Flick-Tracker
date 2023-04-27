396599afe51ba1939cdd39d78db9af0a1f715c97
brandongiordano <bgiordano9917@aol.com>
2023-04-27 15:32:49 -0400

    functioning dashboard template

--- client/src/components/Dashboard/DashFeed.js
+++ client/src/components/Dashboard/DashFeed.js
@@ -2,3 +2,4 @@ import React from 'react'
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -2,8 +2,11 @@ import React, { useState } from 'react';
 import Contianer from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
 import Navigation from "../components/Nav.js";
 import ProfileCard from '../components/ProfileCard';
+import DashFeed from '../components/Dashboard/DashFeed.js';
+import FollowFeed from '../components/Dashboard/FollowFeed.js';
 
 import "../styles/App.css"
 
@@ -14,7 +17,13 @@ const Dashboard = () => {
         <Contianer>
             <Row>
                 <Col><ProfileCard /></Col>
-                <Col><p>2 of 3</p></Col>
+                <Col>
+                 <Row>
+                    <Button>New Reviews</Button>
+                    <Button>Following</Button>
+                 </Row>
+                 <Row><p>2 of 3</p></Row>
+                </Col>
                 <Col><p>3 of 3</p></Col>
             </Row>
         </Contianer> 
