b19e9462b9c7f9eacb53aeab88341a5c3257f4d8
AveryNewhart <newhartreeceavery@gmail.com>
2023-05-01 16:55:38 -0400

    yippie

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
+// let uri = "mongodb+srv://avery:password12345@the-flick-tracker.dnbkejv.mongodb.net/?retryWrites=true&w=majority"
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
