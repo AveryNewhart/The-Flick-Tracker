71715cf3bdcbfe99d8d373ac54792558a2fd74bc
brandongiordano <bgiordano9917@aol.com>
2023-04-30 20:26:29 -0400

    adding new profile files

--- client/src/components/WatchedMovies.js
+++ client/src/components/WatchedMovies.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
 import { QUERY_PROTECTED } from '../utils/queries';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
 // import Auth from "../utils/auth";
 
 
@@ -11,27 +15,47 @@ const WatchedMovies = () => {
   const userData = data?.protected || {};
 
   return (
-    <div>
+    <Container fluid>
+      <Row xs={2} md={5} className="g-4">
       <h2>Watched Movies</h2>
-      {userData.watchedMovies.length ? (
-        <ul>
-          {userData.watchedMovies.map((movie) => (
-            <li key={movie.movieId}>
-              <h3>{movie.title}</h3>
-              {/* <p>Released: {movie.releaseYear}</p> */}
-              <p>{movie.imageURL}</p>
-        <p>overview: {movie.overview}</p>
-        {/* <p>Release Data {movie.releaseYear}</p> */}
-              {/* <p>Director: {movie.director}</p> */}
-              {/* Add more properties as needed */}
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>No watched movies yet.</p>
-      )}
-    </div>
+        {userData.WatchedMovies?.map((movie) => (
+          <Col key={movie.id} style={{ justifyContent: 'center', display: 'flex' }}>
+            <Card style={{ width: '12rem', height: '18rem' }}>
+              <Card.Img variant="top" src={movie.imageURL} />
+              <Card.ImgOverlay>
+                <Card.Body>
+                  <Card.Title>{movie.title}</Card.Title>
+                </Card.Body>
+              </Card.ImgOverlay>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
+
+  // return (
+  //   <div>
+  //     <h2>Watched Movies</h2>
+  //     {userData.watchedMovies.length ? (
+  //       <ul>
+  //         {userData.watchedMovies.map((movie) => (
+  //           <li key={movie.movieId}>
+  //             <h3>{movie.title}</h3>
+  //             {/* <p>Released: {movie.releaseYear}</p> */}
+  //             <p>{movie.imageURL}</p>
+  //       <p>overview: {movie.overview}</p>
+  //       {/* <p>Release Data {movie.releaseYear}</p> */}
+  //             {/* <p>Director: {movie.director}</p> */}
+  //             {/* Add more properties as needed */}
+  //           </li>
+  //         ))}
+  //       </ul>
+  //     ) : (
+  //       <p>No watched movies yet.</p>
+  //     )}
+  //   </div>
+  // );
 };
 
 export default WatchedMovies;
