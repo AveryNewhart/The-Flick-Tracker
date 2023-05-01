58720c2b06485d3764ea52dd7a141525893a1d31
AveryNewhart <newhartreeceavery@gmail.com>
2023-05-01 15:11:04 -0400

    string

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/flicktracker" //! We may need to change this connection string in the future
+  process.env.MONGODB_URI || "mongodb+srv://avery:password12345@the-flick-tracker.dnbkejv.mongodb.net/?retryWrites=true&w=majority" //! We may need to change this connection string in the future
 );
 
 module.exports = mongoose.connection;
+
+
+//  mongodb+srv://avery:<password>@the-flick-tracker.dnbkejv.mongodb.net/?retryWrites=true&w=majority
+// "mongodb://127.0.0.1:27017/flicktracker" //! We may need to change this connection string in the future
