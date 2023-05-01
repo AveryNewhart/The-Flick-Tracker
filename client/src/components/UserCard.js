221277889c78d3ed98d4988ff62b336d0eccd141
brandongiordano <bgiordano9917@aol.com>
2023-05-01 14:07:09 -0400

    add follower almost working

--- client/src/components/UserCard.js
+++ client/src/components/UserCard.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, Button } from 'react-bootstrap';
 import { useParams } from "react-router-dom";
-import { useQuery, gql } from '@apollo/client';
+import { useQuery, useMutation, gql } from '@apollo/client';
 
 const styles = {
   cardStyles: {
     width: '18rem',
     padding: '0.5rem',
-  }
+  },
+  btnStyles: {
+    background: '#c10206',
+    width: '100%',
+    height: '100%',
+    borderStyle: 'none',
+    transition: 'background-color 0.3s ease',
+    '&:hover': {
+      background: '#ff4444',
+    },
+    '&:active': {
+      background: '#990000',
+    },
+  },
 }
+
 
 const QUERY_USER = gql`
 query Query($username: String!) {
@@ -37,32 +51,72 @@ query Query($username: String!) {
   }
 `;
 
+const ADD_FOLLOWER = gql`
+mutation addFollower($userId: ID!, $followedUserId: ID!) {
+  addFollower(userId: $userId, followedUserId: $followedUserId) {
+    id
+  }
+}
+`;
+
+const REMOVE_FOLLOWER = gql`
+mutation removeFollower($userId: ID!, $followedUserId: ID!) {
+  unfollow(userId: $userId, followedUserId: $followedUserId) {
+    id
+  }
+}
+`;
+
 const UserCard = () => {
     const { username } = useParams();
-    console.log(username);
   
     // Query user data
     const { loading, data } = useQuery(QUERY_USER, {
       variables: { username } ,
     });
 
-    console.log(data);
+    const [addFollower] = useMutation(ADD_FOLLOWER);
+    const [removeFollower] = useMutation(REMOVE_FOLLOWER);
+
+    const handleAddFollower = () => {
+      addFollower({
+        variables: {
+          userId: data?.user?.id,
+          followedUserId: "followedUserId", // replace with actual followed user ID
+        },
+      });
+    };
+
+    const handleRemoveFollower = () => {
+      removeFollower({
+        variables: {
+          userId: data?.user?.id,
+          followedUserId: "followedUserId", // replace with actual followed user ID
+        },
+      });
+    };
   
     if (loading) return <p>Loading...</p>;
   
     const user = data?.user;
-    console.log(user)
     if (!user) return <p>User not found</p>;
+
+    const isFollowing = user.followers.some((follower) => follower.id === "currentUserId"); // replace with actual current user ID
   
     return (
-      <Card style={styles.cardStyles}>
-        <Card.Body>
-          <Card.Title>{user.username || 'User Name'}</Card.Title>
-          <Card.Subtitle>Followers: {user.followers.length || 0}</Card.Subtitle>
-          <Card.Subtitle>Following: {user.followings.length || 0}</Card.Subtitle>
-        </Card.Body>
-      </Card>
-    );
-};
-
-export default UserCard;
+        <Card style={styles.cardStyles}>
+          <Card.Body>
+            <Card.Title>{user.username || 'User Name'}</Card.Title>
+            <Card.Subtitle>Followers: {user.followers.length || 0}</Card.Subtitle>
+            <Card.Subtitle>Following: {user.followings.length || 0}</Card.Subtitle>
+            {isFollowing ? (
+              <Button variant="primary" style={styles.btnStyles} onClick={handleRemoveFollower}>Unfollow</Button>
+            ) : (
+              <Button variant="primary" style={styles.btnStyles} onClick={handleAddFollower}>Follow</Button>
+            )}
+          </Card.Body>
+        </Card>
+      );
+  };
+  
+  export default UserCard;
