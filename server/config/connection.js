1a16d6a801ada341707a66521172df398d0a9be1
AveryNewhart <newhartreeceavery@gmail.com>
2023-05-01 18:46:54 -0400

    idk

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 // let uri = "mongodb+srv://avery:password12345@the-flick-tracker.dnbkejv.mongodb.net/?retryWrites=true&w=majority"
 
-const MONGODB_URI = process.env.MONGODB_URI;
+// const MONGODB_URI = process.env.MONGODB_URI;
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/the-flick-tracker',
   {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useFindAndModify: false
+  // useFindAndModify: false
 
   // useCreateIndex: true,
 }
