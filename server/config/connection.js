93b264643c13f0646b0058ca200a829f4f21edac
AveryNewhart <newhartreeceavery@gmail.com>
2023-05-01 15:23:56 -0400

    connection and server

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,23 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb+srv://avery:password12345@the-flick-tracker.dnbkejv.mongodb.net/?retryWrites=true&w=majority" //! We may need to change this connection string in the future
-);
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
 module.exports = mongoose.connection;
+
+
+
+// const mongoose = require("mongoose");
+
+// mongoose.connect(
+//   process.env.MONGODB_URI || "mongodb+srv://avery:password12345@the-flick-tracker.dnbkejv.mongodb.net/?retryWrites=true&w=majority" //! We may need to change this connection string in the future
+// );
+
+// module.exports = mongoose.connection;
 
 
 //  mongodb+srv://avery:<password>@the-flick-tracker.dnbkejv.mongodb.net/?retryWrites=true&w=majority
--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.get("/", (req, res) => {
 });
 
 // Create a new instance of an Apollo server with the GraphQL schema
-const startApolloServer = async (typeDefs, resolvers) => {
+const startApolloServer = async () => {
   await server.start();
   server.applyMiddleware({ app });
 
