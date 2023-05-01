362d8f8ff7020b8d6f4fa7bf204dff7943361106
AveryNewhart <newhartreeceavery@gmail.com>
2023-05-01 19:36:52 -0400

    ugh

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,7 +5,7 @@ const expiration = "2h";
 
 module.exports = {
   authMiddleware: function ({ req }) {
-    let token = req.body.token || req.query.token || req.headers.authorization;
+    let token =  req.query.token || req.headers.authorization;
 
     if (req.headers.authorization) {
       token = token.split(" ").pop().trim();
