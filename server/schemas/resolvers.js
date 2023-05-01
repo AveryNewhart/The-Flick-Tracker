8919b670e84681aeeb598c15fcb5163303898003
Paul Dangelo <pdangelo1988@gmail.com>
2023-04-30 23:02:38 -0400

    addWatchedMovie done

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -131,11 +131,6 @@ const resolvers = {
       // Return the user with the updated followings and followers arrays.
       return followingUser;
     },
-
-    // unfollow: async (parent, { userId, followedUserId }, { user }) => {
-    //   // Authentication check to make sure we have a valid user.
-    //   if (!user) {
-    //     throw new AuthenticationError("You need to be logged in!");
         
     unfollow: async (parent, { userId, followedUserId }, { user }) => {
       // Authentication check to make sure we have a valid user.
@@ -170,17 +165,28 @@ const resolvers = {
       // Return the user with the updated followings and followers arrays.
       return unfollowingUser;
     },
-    // removeWatchedMovie: (parent, args, context) => {
-    //   // Remove the Watched object with the provided movieId from the User's watchedMovies array
-    //   // Return the removed Watched object
-    // },
+    removeWatchedMovie: async (parent, { movie, input }, context) => {
+      if (context.user) {
+        console.log(input)
+
+         const dataMovie = await User.findOneAndUpdate(
+          { movie },
+          { $pull: { watchedMovies: { _id: context.user._id, input } } },
+          { new: true, runValidators: true }
+        ).populate(watchedMovies)
+        
+          return dataMovie
+      }
+      throw new AuthenticationError("must be logged in to perform this action");
+    },
     addWatchedMovie: async (parent, { movie }, context) => {
       if (context.user) {
         return (updatedUser = await User.findOneAndUpdate(
           { _id: context.user._id },
           { $addToSet: { watchedMovies: movie } },
-          { new: true, runValidators: true }
-        ));
+          { new: true, runValidators: true },
+        ).populate("watchedMovies")
+        );
       }
       throw new AuthenticationError("You need to be logged in!");
     },
@@ -199,24 +205,24 @@ const resolvers = {
         { _id: context.user._id },
         { $addToSet: { watchlist: movie._id } },
         { new: true }
-      ).populate("watchlist");
+      ).populate("watchlist")
       return user;
     },
     // removeMovieFromWatchlist: (parent, args, context) => {
     //   // Remove the Watchlist object with the provided movieId from the User's watchlist array
     //   // Return the removed Watchlist object
     // },
-    // addReview: async (_, { movieId, text, rating }, { dataSources, auth }) => {
-    //   console.log(auth);
-    //   const input = {
-    //     movie: movieId,
-    //     auth, //!Structuring data we're passing to Review.create
-    //     text,
-    //     rating,
-    //   };
-    //   const newReview = await Review.create(input);
-    //   return newReview;
-
+    addReview: async (_, { movieId, text, rating }, context) => {
+      console.log(auth);
+      const input = {
+        movie: movieId,
+        auth, //!Structuring data we're passing to Review.create
+        text,
+        rating,
+      };
+      const newReview = await Review.create(input);
+      return newReview;
+    }
     //   // Create a new Review object for the provided User and Movie objects with the provided input
     //   // Return the new Review object
     // },
@@ -288,5 +294,5 @@ const resolvers = {
     //   },
     // }
   },
-};
+}
 module.exports = resolvers;
